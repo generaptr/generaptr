9122a4aa85be7d263c400a66e737637559ccb09b
Cupsa Darius <cupsadarius@gmail.com>
2017-03-25 22:07:42 +0200

    fix test add slack notification

--- test/mysql/mysql.js
+++ test/mysql/mysql.js
@@ -36,9 +36,8 @@ describe('print', () => {
       }).then((schema) => {
         assert.equal(1, schema.length);
         const table = schema.pop();
-        console.log(table);
         assert.equal('users', table.name);
-        assert.equal(4, table.columns.length);
+        assert.equal(4, Object.keys(table.columns).length);
         done();
       }).catch(err => {console.log(err); assert.fail(), done()});
     } catch (e) {
