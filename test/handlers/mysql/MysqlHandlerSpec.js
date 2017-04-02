ad49c185ecea45792d897e614b096935ab63bd0d
Cupsa Darius <cupsadarius@gmail.com>
2017-04-02 19:24:41 +0300

    update tests

---
+++ test/handlers/mysql/MysqlHandlerSpec.js
@@ -0,0 +1,112 @@
+const assert = require('assert');
+const MysqlHandler = require('../../../src/handlers/MysqlHandler');
+
+const validConnectionData = {
+  host: '172.16.140.134',
+  port: 3306,
+  user: 'root',
+  password: 'secret',
+  database: 'test'
+};
+
+describe('Mysql database handler', () => {
+  it('should create a valid handler', () => {
+    try {
+      const handler = new MysqlHandler(validConnectionData);
+      assert.equal(validConnectionData, handler.options);
+    } catch (e) {
+      assert.fail();
+    }
+  });
+
+  it('should connect to the database', () => {
+    try {
+      const handler = new MysqlHandler(validConnectionData);
+
+      handler.connect();
+      assert.notEqual(null, handler.connection);
+      handler.close();
+
+    } catch (e) {
+      assert.fail();
+    }
+  });
+
+  it('should return an array with the table names', (done) => {
+    try {
+      const handler = new MysqlHandler(validConnectionData);
+      handler.connect();
+      handler.readTables().then(tables => {
+        assert.equal(2, tables.length);
+        handler.close();
+        done();
+      });
+    } catch (e) {
+      assert.fail(e);
+      done();
+    }
+  });
+
+  it('should return a valid table schema', (done) => {
+    try {
+      const handler = new MysqlHandler(validConnectionData);
+      handler.connect();
+
+      handler.getTableSchema('users').then(schema => {
+
+        assert.equal('users', schema.name);
+        assert.equal(4, Object.keys(schema.columns).length);
+
+        handler.close();
+        done();
+      });
+
+    } catch (e) {
+      assert.fail();
+      done();
+    }
+  });
+
+  it('should detect a table relation', (done) => {
+    try {
+      const handler = new MysqlHandler(validConnectionData);
+      handler.connect();
+      handler.getRelationsForTable('accounts').then(relations => {
+        assert.equal('users', relations['user_id'].table);
+        assert.equal('id', relations['user_id'].column);
+
+        handler.close();
+        done();
+      });
+    } catch (e) {
+      assert.fail();
+      done();
+    }
+  });
+
+  it('should return a valid database schema', (done) => {
+    try {
+      const handler = new MysqlHandler(validConnectionData);
+      handler.connect();
+      handler.readTables().then(tables => {
+        handler.readSchema(tables).then(schema => {
+          assert.equal(2, schema.length);
+
+          const users = schema.filter(table => table.name  === 'users').pop();
+          const accounts = schema.filter(table => table.name === 'accounts').pop();
+
+          assert.equal('users', users.name);
+          assert.equal(4, Object.keys(users.columns).length);
+          assert.equal('accounts', accounts.name);
+          assert.equal(3, Object.keys(accounts.columns).length);
+
+          handler.close();
+          done();
+        });
+      });
+    } catch (e) {
+      assert.fail();
+      done();
+    }
+  });
+});
---
+++ test/validators/MysqlConnectionValidatorSpec.js
@@ -0,0 +1,60 @@
+const assert = require('assert');
+const mysqlConnectionValidator = require('../../src/validators/MysqlConnectionValidator');
+
+describe('Mysql connection validator', () => {
+  it('should throw an error without a host provided', () => {
+    try {
+      mysqlConnectionValidator.isValid({});
+    } catch (e) {
+      assert.equal('Host not provided.', e.message);
+    }
+  });
+
+  it('should throw an error without a valid port provided', () => {
+    try {
+      mysqlConnectionValidator.isValid({
+        host: '127.0.0.1',
+        port: null
+      });
+    } catch (e) {
+      assert.equal('Valid port not provided.', e.message);
+    }
+  });
+
+  it('should throw an error without a database provided', () => {
+    try {
+      mysqlConnectionValidator.isValid({
+        host: '127.0.0.1',
+        port: 3306
+      });
+    } catch (e) {
+      assert.equal('Database not provided.', e.message);
+    }
+  });
+
+  it('should throw an error without a user provided', () => {
+    try {
+      mysqlConnectionValidator.isValid({
+        host: '127.0.0.1',
+        port: 3306,
+        database: 'test'
+      });
+    } catch (e) {
+      assert.equal('User not provided.', e.message);
+    }
+  });
+
+  it('should not throw any errors with valid data', () => {
+    try {
+      assert.equal(true, mysqlConnectionValidator.isValid({
+        host: '127.0.0.1',
+        port: 3306,
+        database: 'test',
+        user: 'root',
+        password: ''
+      }));
+    } catch (e) {
+      assert.fail();
+    }
+  });
+});
