8119fd48c8f3a32b6c0a691f7dc9b7a8496fc0d0
Cupsa Darius <cupsadarius@gmail.com>
2017-04-02 20:01:51 +0300

    Detect foreign keys between tables

--- src/validators/MysqlConnectionValidator.js
+++ src/validators/MysqlConnectionValidator.js
@@ -6,22 +6,22 @@ class MysqlConnectionValidator extends BaseValidator {
     let valid = true;
     if (!(data && data.host && typeof data.host === 'string')) {
       valid = false;
-      this.trigger('Host not provided');
+      this.trigger('Host not provided.');
     }
 
     if (!(data && data.port && !isNaN(parseInt(data.port)))) {
       valid = false;
-      this.trigger('Valid port not provided');
+      this.trigger('Valid port not provided.');
     }
 
     if (!(data && data.database && typeof data.database === 'string')) {
       valid = false;
-      this.trigger('Database not provided');
+      this.trigger('Database not provided.');
     }
 
     if (!(data && data.user && typeof data.user === 'string')) {
       valid = false;
-      this.trigger('User not provided');
+      this.trigger('User not provided.');
     }
 
     return valid;
---
+++ test/handlers/mysql/MysqlHandlerSpec.js
@@ -0,0 +1,122 @@
+const assert = require('assert');
+const MysqlHandler = require('../../../src/handlers/MysqlHandler');
+
+const validConnectionData = {
+  host: '127.0.0.1',
+  port: 3306,
+  user: 'root',
+  password: '',
+  database: 'test'
+};
+
+describe('Mysql database handler', () => {
+  let handler = null;
+
+  beforeEach(() => {
+    handler = new MysqlHandler(validConnectionData);
+    handler.connect();
+  });
+
+  afterEach(() => {
+    handler.close();
+    handler = null;
+  });
+
+  it('should create a valid handler', () => {
+    try {
+      const handler = new MysqlHandler(validConnectionData);
+      assert.equal(validConnectionData, handler.options);
+    } catch (e) {
+      assert.fail();
+    }
+  });
+
+  it('should connect to the database', () => {
+    try {
+      assert.notEqual(null, handler.connection);
+    } catch (e) {
+      assert.fail();
+    }
+  });
+
+  it('should return an array with the table names', (done) => {
+    try {
+      handler.readTables().then(tables => {
+        assert.equal(2, tables.length);
+        done();
+      }).catch(err => {
+        console.log(err);
+        assert.fail();
+        done();
+      });
+    } catch (e) {
+      assert.fail(e);
+      done();
+    }
+  });
+
+  it('should return a valid table schema', (done) => {
+    try {
+      handler.getTableSchema('users').then(schema => {
+
+        assert.equal('users', schema.name);
+        assert.equal(4, Object.keys(schema.columns).length);
+
+        done();
+      }).catch(err => {
+        console.log(err);
+        assert.fail();
+        done();
+      });
+
+    } catch (e) {
+      assert.fail();
+      done();
+    }
+  });
+
+  it('should detect a table relation', (done) => {
+    try {
+      handler.getRelationsForTable('accounts').then(relations => {
+        assert.equal('users', relations['user_id'].table);
+        assert.equal('id', relations['user_id'].column);
+
+        done();
+      }).catch(err => {
+        console.log(err);
+        assert.fail();
+        done();
+      });
+    } catch (e) {
+      assert.fail();
+      done();
+    }
+  });
+
+  it('should return a valid database schema', (done) => {
+    try {
+      handler.readTables().then(tables => {
+        handler.readSchema(tables).then(schema => {
+          assert.equal(2, schema.length);
+
+          const users = schema.filter(table => table.name  === 'users').pop();
+          const accounts = schema.filter(table => table.name === 'accounts').pop();
+
+          assert.equal('users', users.name);
+          assert.equal(4, Object.keys(users.columns).length);
+          assert.equal('accounts', accounts.name);
+          assert.equal(3, Object.keys(accounts.columns).length);
+
+          done();
+        }).catch(err => {
+          console.log(err);
+          assert.fail();
+          done();
+        });
+      });
+    } catch (e) {
+      assert.fail();
+      done();
+    }
+  });
+});
