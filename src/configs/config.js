a42a9aeb7252bfb42cc07087db6175fdb282c8c5
Cupsa Darius <cupsadarius@gmail.com>
2017-07-07 22:30:34 +0300

    fix lint issues

--- src/configs/config.js
+++ src/configs/config.js
@@ -1,3 +1,6 @@
+const env = require('get-env')({
+  test: ['test'],
+});
 /**
  * Application configurations
  * @type {{DEFAULT_DEPTH_LEVEL: number}}
@@ -10,22 +13,22 @@ module.exports = {
   DEFAULT_MAX_DEPTH_LEVEL: 2,
   DEFAULT_ARRAY_LENGTH: 2,
   DEPTH_INCREMENT: 1,
-  ENV: process.env.NODE_ENV || 'development',
+  ENV: env,
   CONNECTION_INFO: {
     MYSQL: {
-      development: {
+      dev: {
         host: '192.168.99.100',
         port: 3306,
         user: 'root',
         password: 'secret',
-        database: 'test'
+        database: 'test',
       },
       test: {
         host: '127.0.0.1',
         port: 3306,
         user: 'root',
         password: '',
-        database: 'test'
+        database: 'test',
       },
     },
   },
