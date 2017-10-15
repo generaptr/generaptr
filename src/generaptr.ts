e05476c18b042a7ce673f6e6a98e4b402c1c932d
Cupsa Darius <cupsadarius@gmail.com>
2017-10-15 20:48:53 +0300

    chore(lint): fix minor lint issues

--- src/generaptr.ts
+++ src/generaptr.ts
@@ -3,12 +3,12 @@ import RamlFileOperations from './fileOperations/RamlFileOperations';
 import MysqlHandler from './handlers/MysqlHandler';
 import RamlHandler from './handlers/RamlHandler';
 
-export const operations = {
+export const operations: {api: Object; raml: Object} = {
   api: ApiFileOperations,
   raml: RamlFileOperations,
 };
 
-export const handlers = {
+export const handlers: {MySql: Object; RAML: Object} = {
   MySql: MysqlHandler,
   RAML: RamlHandler,
 };
