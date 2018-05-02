8180df1d925c93d283f9eb8b8da6eddf75bd3ea3
Cupsa Darius <cupsadarius@gmail.com>
2018-05-02 22:02:04 +0300

    [skip ci] add tests for the handler

--- src/handlers/mysql/Handler.ts
+++ src/handlers/mysql/Handler.ts
@@ -1,5 +1,5 @@
 import HandlerInterface from '../interfaces/HandlerInterface';
-import MySqlParser from './Parser';
+import Parser from './Parser';
 import * as mysql from 'mysql2/promise';
 import Schema from '../../types/Schema';
 import Table from '../../types/Table';
@@ -13,126 +13,104 @@ export default class Handler implements HandlerInterface<RawMySqlColumn> {
   private targetDatabase: String;
   private connection: mysql.Connection;
   private options: mysql.ConnectionOptions;
-  private parser: MySqlParser;
+  private parser: Parser;
 
   public constructor(options: mysql.ConnectionOptions) {
-    this.parser = new MySqlParser();
+    this.parser = new Parser();
     this.targetDatabase = String(options.database);
     this.options = options;
-
-    return this;
   }
 
-  public getParser(): MySqlParser {
+  public getParser(): Parser {
     return this.parser;
   }
 
   public async getSchema(): Promise<Schema> {
-    try {
-      await this.connect();
+    await this.connect();
 
-      const tableNames: string[] = await this.getTableNames();
-      const tables: Table[] = await Promise.all(
-        tableNames.map(
-          async (name: string) => this.getTable(name),
-        ),
-      );
-      const schema: Schema = (new Schema()).setTables(tables);
-      await this.connection.end();
+    const tableNames: string[] = await this.getTableNames();
+    const tables: Table[] = await Promise.all(
+      tableNames.map(
+        async (name: string) => this.getTable(name),
+      ),
+    );
+    const schema: Schema = (new Schema()).setTables(tables);
+    await this.connection.end();
 
-      return this.parser.normalizeRelations(schema);
-
-    } catch (error) {
-      throw error;
-    }
+    return this.parser.normalizeRelations(schema);
   }
 
   private async connect(): Promise<boolean> {
-    try {
-      const connection: mysql.Connection = await mysql.createConnection({
-        ...this.options,
-        database: 'information_schema',
-      });
+    const connection: mysql.Connection = await mysql.createConnection({
+      ...this.options,
+      database: 'information_schema',
+    });
 
-      this.connection = connection;
-      
-      return true;
-    } catch (error) {
-      throw error;
-    }
+    this.connection = connection;
+    
+    return true;
   }
 
   private async getTableNames(): Promise<string[]> {
-    try {
-      const [rows] = await this.connection.execute('SELECT TABLE_NAME FROM TABLES WHERE TABLE_SCHEMA = ?', [this.targetDatabase]);
+    const [rows] = await this.connection.execute('SELECT TABLE_NAME FROM TABLES WHERE TABLE_SCHEMA = ?', [this.targetDatabase]);
 
-      return (rows as mysql.RowDataPacket[]).map((row: mysql.RowDataPacket) => row.TABLE_NAME);
-    } catch (error) {
-      throw error;
-    }
+    return (rows as mysql.RowDataPacket[]).map((row: mysql.RowDataPacket) => row.TABLE_NAME);
   }
 
   private async getTable(name: string): Promise<Table> {
-    try {
-      const [rows] = await this.connection.execute(
-        'SELECT COLUMN_NAME, IS_NULLABLE, DATA_TYPE, CHARACTER_MAXIMUM_LENGTH, COLUMN_KEY, COLUMN_TYPE FROM COLUMNS WHERE TABLE_SCHEMA = ? AND TABLE_NAME = ?',
-        [this.targetDatabase,  name],
-      );
-      const relations: TableReference[] = await this.getTableRelations(name);
-      const columns: Column[] = (rows as mysql.RowDataPacket[]).map((row: mysql.RowDataPacket) => {
-        const column: Column = this.parser.normalizeColumn({
-          COLUMN_NAME: row.COLUMN_NAME,
-          COLUMN_KEY: row.COLUMN_KEY,
-          COLUMN_TYPE: row.COLUMN_TYPE,
-          IS_NULLABLE: row.IS_NULLABLE,
-          DATA_TYPE: row.DATA_TYPE,
-        });
-        
-        const relation: TableReference | undefined = relations.find(
-          (rel: TableReference) => rel.name === column.getName(),
-        );
-        if (relation) {
-          const foreignKey: ForeignKey = new ForeignKey();
-          foreignKey
-            .setTarget({table: relation.table, column: relation.column})
-            .setSource({table: name, column: relation.name})
-            .setAlias(similarity(column.getName(), relation.table) < 0.5);
-          column
-            .setForeignKey(foreignKey)
-            .setType(column.getType().setType(toTitleCase(relation.table)))
-            .setName((rows as mysql.RowDataPacket[]).length === relations.length ? relation.name : toColumnName(relation.name));
-        }
-
-        return column;
+    const [rows] = await this.connection.execute(
+      'SELECT COLUMN_NAME, IS_NULLABLE, DATA_TYPE, CHARACTER_MAXIMUM_LENGTH, COLUMN_KEY, COLUMN_TYPE FROM COLUMNS WHERE TABLE_SCHEMA = ? AND TABLE_NAME = ?',
+      [this.targetDatabase,  name],
+    );
+    const relations: TableReference[] = await this.getTableRelations(name);
+    const columns: Column[] = (rows as mysql.RowDataPacket[]).map((row: mysql.RowDataPacket) => {
+      const column: Column = this.parser.normalizeColumn({
+        COLUMN_NAME: row.COLUMN_NAME,
+        COLUMN_KEY: row.COLUMN_KEY,
+        COLUMN_TYPE: row.COLUMN_TYPE,
+        IS_NULLABLE: row.IS_NULLABLE,
+        DATA_TYPE: row.DATA_TYPE,
       });
-      const table: Table = new Table();
+      
+      const relation: TableReference | undefined = relations.find(
+        (rel: TableReference) => rel.name === column.getName(),
+      );
+      if (relation) {
+        const foreignKey: ForeignKey = new ForeignKey();
+        foreignKey
+          .setTarget({table: relation.table, column: relation.column})
+          .setSource({table: name, column: relation.name})
+          .setAlias(similarity(column.getName(), relation.table) < 0.5);
+        column
+          .setForeignKey(foreignKey)
+          .setType(column.getType().setType(toTitleCase(relation.table)))
+          .setName((rows as mysql.RowDataPacket[]).length === relations.length ? relation.name : toColumnName(relation.name));
+      }
 
-      return table
-        .setName(name)
-        .setColumns(columns)
-        .setCrossReference(columns.length === relations.length)
-        .setForeignKeys(relations.length > 0);
-        
-    } catch (error) {
-      throw error;
-    }
+      return column;
+    });
+    const table: Table = new Table();
+
+    return table
+      .setName(name)
+      .setColumns(columns)
+      .setCrossReference(columns.length === relations.length)
+      .setForeignKeys(relations.length > 0);
+      
   }
 
   private async getTableRelations(name: string): Promise<TableReference[]> {
-    try {
-      const [rows] = await this.connection.execute(
-        'SELECT COLUMN_NAME, rc.REFERENCED_TABLE_NAME, REFERENCED_COLUMN_NAME FROM REFERENTIAL_CONSTRAINTS rc JOIN KEY_COLUMN_USAGE cu ON cu.CONSTRAINT_NAME = rc.CONSTRAINT_NAME WHERE rc.CONSTRAINT_SCHEMA = ? AND rc.TABLE_NAME = ?',
-        [this.targetDatabase, name],
-      );
+    const [rows] = await this.connection.execute(
+      'SELECT COLUMN_NAME, rc.REFERENCED_TABLE_NAME, REFERENCED_COLUMN_NAME FROM REFERENTIAL_CONSTRAINTS rc JOIN KEY_COLUMN_USAGE cu ON cu.CONSTRAINT_NAME = rc.CONSTRAINT_NAME WHERE rc.CONSTRAINT_SCHEMA = ? AND rc.TABLE_NAME = ?',
+      [this.targetDatabase, name],
+    );
 
-      return (rows as mysql.RowDataPacket[]).map((row: mysql.RowDataPacket) =>
-        ({
-          name: row.COLUMN_NAME,
-          table: row.REFERENCED_TABLE_NAME,
-          column: row.REFERENCED_COLUMN_NAME,
-        }));
-    } catch (error) {
-      throw error;
-    }
+    return (rows as mysql.RowDataPacket[]).map((row: mysql.RowDataPacket) =>
+      ({
+        name: row.COLUMN_NAME,
+        table: row.REFERENCED_TABLE_NAME,
+        column: row.REFERENCED_COLUMN_NAME,
+      })
+    );
   }
 }
---
+++ test/handlers/mysql/Handler.spec.ts
@@ -0,0 +1,36 @@
+import * as assert from 'assert';
+import Handler from '../../../src/handlers/mysql/Handler';
+import Parser from '../../../src/handlers/mysql/Parser';
+
+describe('MySql Handler', async () => {
+  const options = {
+    host: '127.0.0.1',
+    port: 3306,
+    user: 'root',
+    password: 'secret',
+    database: 'test',
+  };
+
+  it('should initiate the right handler', () => {
+    assert.equal(new Handler(options) instanceof Handler, true);
+  });
+
+  it('should use the proper parser', () => {
+    assert.equal((new Handler(options)).getParser() instanceof Parser, true);
+  });
+
+  it('should fail to connect with bad data', async () => {
+    try {
+      const instance = new Handler({...options, password: 'not-the-right-password'});
+      await instance.getSchema();
+    } catch (error) {
+      assert.equal(error.message, `Access denied for user 'root'@'172.17.0.1' (using password: YES)`);
+    }
+  });
+
+  it('should properly return a valid schema', async () => {
+    const instance = new Handler(options);
+    const schema = await instance.getSchema();
+    assert.equal(schema.getTables().length, 5);
+  });
+});
